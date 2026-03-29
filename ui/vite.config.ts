75dbdbc889c75d756ef003c22cb3c183ed20e229
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-03-29 10:50:48 +0530

    fix: proxy /media to server

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
       },
+      "/media": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
     },
   },
 });
