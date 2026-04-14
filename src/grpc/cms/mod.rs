a87cc9d29a3a1a75a2e6fa88d9dbee475ab3e5d0
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-04-14 13:39:11 +0530

    feat(grpc): update cms module to expose unified v1 package

--- src/grpc/cms/mod.rs
+++ src/grpc/cms/mod.rs
@@ -1,11 +1,3 @@
-pub mod site {
-    pub mod v1 {
-        include!("cms.site.v1.rs");
-    }
-}
-
-pub mod admin {
-    pub mod v1 {
-        include!("cms.admin.v1.rs");
-    }
+pub mod v1 {
+    include!("cms.v1.rs");
 }
