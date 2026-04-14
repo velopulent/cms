0b2295e6744c23779748f1a693f30728f6b91573
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-04-14 13:41:05 +0530

    feat(router): update graphql routes for unified schema

--- src/router/graphql.rs
+++ src/router/graphql.rs
@@ -8,7 +8,7 @@ use std::sync::Arc;
 
 use crate::config::Config;
 use crate::graphql::context::GqlContext;
-use crate::graphql::schema::{AdminCmsSchema, CmsSchema, build_admin_schema, build_schema};
+use crate::graphql::schema::{CmsSchema, build_schema};
 use crate::handlers::file_handler::StorageManager;
 use crate::repository::Repository;
 
@@ -28,40 +28,16 @@ async fn graphql_handler(
     async_graphql_axum::GraphQLResponse::from(response)
 }
 
-async fn admin_graphql_handler(
-    axum::extract::Extension(schema): axum::extract::Extension<Arc<AdminCmsSchema>>,
-    axum::extract::Extension(repository): axum::extract::Extension<Repository>,
-    axum::extract::Extension(storage): axum::extract::Extension<StorageManager>,
-    axum::extract::Extension(config): axum::extract::Extension<Config>,
-    headers: HeaderMap,
-    req: async_graphql_axum::GraphQLRequest,
-) -> async_graphql_axum::GraphQLResponse {
-    let auth_header = headers.get("Authorization").and_then(|v| v.to_str().ok());
-    let gql_ctx = GqlContext::from_request(repository, storage, auth_header, &config.hmac_secret).await;
-    let response = schema.execute(req.into_inner().data(gql_ctx)).await;
-    async_graphql_axum::GraphQLResponse::from(response)
-}
-
 async fn graphiql_handler() -> impl IntoResponse {
     Html(
         async_graphql::http::GraphiQLSource::build()
-            .endpoint("/api/graphql/site")
-            .finish(),
-    )
-}
-
-async fn admin_graphiql_handler() -> impl IntoResponse {
-    Html(
-        async_graphql::http::GraphiQLSource::build()
-            .endpoint("/api/graphql/admin")
+            .endpoint("/api/graphql")
             .finish(),
     )
 }
 
 pub fn graphql_routes() -> Router {
     Router::new()
-        .route("/api/graphql/site", get(graphiql_handler).post(graphql_handler))
-        .route("/api/graphql/admin", get(admin_graphiql_handler).post(admin_graphql_handler))
+        .route("/api/graphql", get(graphiql_handler).post(graphql_handler))
         .layer(Extension(Arc::new(build_schema())))
-        .layer(Extension(Arc::new(build_admin_schema())))
 }
