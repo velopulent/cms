d31fa2890f8d930b128f59bc59bc6143e368c8e1
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-06-12 07:48:20 +0530

    feat: update shadcn theme, fonts and components

--- apps/dashboard/src/components/ui/table.tsx
+++ apps/dashboard/src/components/ui/table.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -57,7 +55,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted",
         className
       )}
       {...props}
